d980e7fd30c4263c1b1a68b843f7b9be556a9eaa
Jesse Shapiro <jesse.brian.shapiro@gmail.com>
2019-08-12 14:03:14 -0400

    Created map with FSA marker

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('hello world')
+const mapboxgl = require("mapbox-gl");
+let FullstackNYCoord = [-74.009151, 40.705086];
+let FullstackChiCoord = [-87.641, 41.895];
+
+mapboxgl.accessToken = "pk.eyJ1IjoiamVzc2Utc2hhcGlybyIsImEiOiJjano4bW9yc3QwcG1pM2NtaGJpcHp0OHZ1In0.9R1-eiCpHddydHRwyADU1w";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+let firstMarker = document.createElement("div");
+firstMarker.style.width = "32px";
+firstMarker.style.height = "39px";
+firstMarker.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+new mapboxgl.Marker(firstMarker).setLngLat(FullstackNYCoord).addTo(map);
+
+// new mapboxgl.Marker(document.getElementById('test')).setLngLat(FullstackNYCoord).addTo(map);
+
+// const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+// markerDomEl.style.width = "32px";
+// markerDomEl.style.height = "39px";
+// markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+// new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
